e99ab433dd2449b12a1140f3dcdb17d1f40aeed5
sitraka <sitrakah26@gmail.com>
2024-05-22 21:50:14 +0300

    cors

--- index.js
+++ index.js
@@ -1,40 +1,38 @@
+const swaggerUi = require("swagger-ui-express");
 
+const express = require("express");
+const app = express();
+const port = 3000;
+const sequelize = require("./config/initSequelize");
+const bodyparser = require("body-parser");
+const Article = require("./models/article.js");
+const Utilisateur = require("./models/utilisateur.js");
 
-const swaggerUi = require('swagger-ui-express');
-
-const express = require('express')
-const app = express()
-const port = 3000
-const sequelize = require('./config/initSequelize')
-const bodyparser = require('body-parser')
-const Article = require('./models/article.js')
-const Utilisateur = require('./models/utilisateur.js')
-
-const utilisateurRouter = require('./routes/utilisateurRoute.js')
-const articleRouter = require('./routes/articleRouter.js')
+const utilisateurRouter = require("./routes/utilisateurRoute.js");
+const articleRouter = require("./routes/articleRouter.js");
+const cors = require("cors");
 
 app.use(bodyparser.json());
 const swaggerDocument = require("./swagger.json");
 
-
+app.use(cors())
 //Use routes
-app.use('/soa' , utilisateurRouter);
-app.use('/soa' , articleRouter) ;
-app.use('/api-doc',swaggerUi.serve, swaggerUi.setup(swaggerDocument));
+app.use("/soa", utilisateurRouter);
+app.use("/soa", articleRouter);
+app.use("/api-doc", swaggerUi.serve, swaggerUi.setup(swaggerDocument));
 app.listen(port, () => {
   console.log(`Server is running on port ${port}`);
 });
 
-sequelize.sync().then(() => {
-  console.log('Base de données connectée et modèles synchronisés.');
-  
-}).catch(err => {
-  console.error('Erreur de synchronisation des modèles :', err);
-});
-
-
+sequelize
+  .sync()
+  .then(() => {
+    console.log("Base de données connectée et modèles synchronisés.");
+  })
+  .catch((err) => {
+    console.error("Erreur de synchronisation des modèles :", err);
+  });
 
 // app.get('/', (req, res) => {
 //   res.send('Hello World')
 // })
-
